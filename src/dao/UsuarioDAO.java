415d7e5287845be4d83e21ddf13d88d80e3ae660
Gustavo <gustalimaze@gmail.com>
2025-05-11 17:14:40 -0300

    Codigo do UsuarioDAO

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -22,46 +22,5 @@ public class UsuarioDAO {
         this.con = Conexao.conectar(); // Conexão estabelecida
     }
     
-    public void cadastrarUsuario(Usuario usu) {
-        String sql = "INSERT INTO usuarios (nome, usuario, senha) VALUES (?, ?, ?)";
-             
-        try (PreparedStatement ps = con.prepareStatement(sql)) {
-            // Preenche os parâmetros
-            ps.setString(1, usu.getNome());
-            ps.setString(2, usu.getUsuario());
-            ps.setString(3, usu.getSenha());
-            
-            // Executa e fecha
-            ps.execute();
-            ps.close();
-            
-            JOptionPane.showMessageDialog(null, "Usuário cadastrado com sucesso!");
-            
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + e.getMessage());
-        }
-       } 
-    
-    public Usuario logarUsuario(String usuario, String senha) {
-    String sql = "SELECT id, nome, usuario FROM usuarios WHERE usuario = ? AND senha = ?";
-    
-    try (PreparedStatement ps = con.prepareStatement(sql)) {
-        ps.setString(1, usuario);
-        ps.setString(2, senha);
-        
-        // Modificação principal está aqui ↓
-        try (ResultSet rs = ps.executeQuery()) {
-            if (rs.next()) {
-                Usuario usuarioLogado = new Usuario();
-                usuarioLogado.setId(rs.getInt("id"));
-                usuarioLogado.setNome(rs.getString("nome"));
-                usuarioLogado.setUsuario(rs.getString("usuario"));
-                return usuarioLogado;
-            }
-        }
-    } catch (Exception e) {
-        JOptionPane.showMessageDialog(null, "Erro ao fazer login: " + e.getMessage());
-    }
-    return null;
-}
+
 }
