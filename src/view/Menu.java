607751eeb0779ca2d3e0b663593a8837ade579b6
Gustavo <gustalimaze@gmail.com>
2025-05-11 13:36:31 -0300

    Interface de menu

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -28,6 +28,7 @@ public class Menu extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         menuLabel = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -37,6 +38,9 @@ public class Menu extends javax.swing.JFrame {
         menuLabel.setForeground(new java.awt.Color(0, 204, 0));
         menuLabel.setText("SpotiFEI");
 
+        jLabel1.setForeground(new java.awt.Color(51, 255, 51));
+        jLabel1.setText("Tela inicial");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -45,13 +49,19 @@ public class Menu extends javax.swing.JFrame {
                 .addContainerGap(146, Short.MAX_VALUE)
                 .addComponent(menuLabel)
                 .addGap(144, 144, 144))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(179, 179, 179)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(24, 24, 24)
                 .addComponent(menuLabel)
-                .addContainerGap(241, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addContainerGap(207, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -104,6 +114,7 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel menuLabel;
     // End of variables declaration//GEN-END:variables
