a66d246bdeef5fe084d24a54aee42956c01a4cd0
Gustavo <gustalimaze@gmail.com>
2025-05-19 14:28:35 -0300

    Criada a classe PlayList

--- src/model/PlayList.java
+++ src/model/PlayList.java
@@ -4,10 +4,58 @@
  */
 package model;
 
+import java.util.List;
+
 /**
  *
  * @author gusta
  */
 public class PlayList {
+    private int id;
+    private String nome;
+    private int usuarioId; // ID do usuário dono da playlist
+    private List<Musica> musicas; // Lista de músicas da playlist
+
+    public PlayList() {
+    }
+
+    public PlayList(String nome, int usuarioId) {
+        this.nome = nome;
+        this.usuarioId = usuarioId;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+    
     
 }
