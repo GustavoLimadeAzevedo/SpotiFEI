22c29727cb04af1067775eb68d5f56b9f47818bc
Gustavo <gustalimaze@gmail.com>
2025-05-19 14:24:48 -0300

    Modificações na classe Musica.java

--- src/model/Musica.java
+++ src/model/Musica.java
@@ -10,29 +10,54 @@ package model;
  */
 //
 public class Musica {
-    private int id;
+   private int id;
     private String titulo;
-    private String artista;    
-    private String genero; // Adicione se precisar
-    
-    // Construtor
+    private String artista;
+    private String genero;
+
+    // Construtor vazio necessário
+    public Musica() {
+    }
+
+    // Construtor com parâmetros
     public Musica(int id, String titulo, String artista, String genero) {
         this.id = id;
         this.titulo = titulo;
         this.artista = artista;
         this.genero = genero;
     }
-    
-    // Getters (os setters são opcionais se não for editar)
-    public int getId() { return id; }
-    public String getTitulo() { return titulo; }
-    public String getArtista() { return artista; }
-    public String getGenero() { return genero; }
-    
-    // Método útil para exibição
-    @Override
-    public String toString() {
-        return titulo + " - " + artista + " (" + genero + ")";
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
     
 }
