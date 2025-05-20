e20f45105e9b41fc1f0aaa101a5336f8dcee154b
Gustavo <gustalimaze@gmail.com>
2025-05-20 20:19:40 -0300

    Implementado a Model da classe Admin

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -8,6 +8,55 @@ package model;
  *
  * @author gusta
  */
-public class Adminstrador {
-    
+public class Admin {
+    private int id;
+    private String nome;
+    private String usuario;
+    private String senha;
+
+    public Admin() {
+    }
+
+    public Admin(String nome, String usuario, String senha) {
+        this.nome = nome;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
+
+
+
+
+
