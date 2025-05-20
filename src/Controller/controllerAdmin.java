f28f8f46398b5dffca7743098ce2f77c163c8f3d
Gustavo <gustalimaze@gmail.com>
2025-05-20 20:11:39 -0300

    Implementado o controllerAdmin

--- src/Controller/controllerAdmin.java
+++ src/Controller/controllerAdmin.java
@@ -4,10 +4,27 @@
  */
 package Controller;
 
+import dao.AdminDAO;
+import model.Usuario;
+import java.util.List;
+import model.Admin;
+
 /**
  *
  * @author gusta
  */
 public class controllerAdmin {
-    
+   private AdminDAO adminDAO;
+
+    public controllerAdmin() {
+        this.adminDAO = new AdminDAO();
+    }
+
+    public void cadastrarAdministrador(Admin administrador) {
+        adminDAO.cadastrarAdministrador(administrador);
+    }
+
+    public Admin logarAdministrador(String usuario, String senha) {
+        return adminDAO.logarAdministrador(usuario, senha);
+    }
 }
